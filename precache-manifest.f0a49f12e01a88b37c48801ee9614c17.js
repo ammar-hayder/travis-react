768a2f9c2a983d84877186514c9eee4fece83243
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-25 16:45:42 +0000

    Deploy ammar-hayder/travis-react to github.com/ammar-hayder/travis-react.git:gh-pages

---
+++ precache-manifest.f0a49f12e01a88b37c48801ee9614c17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde5d27276125ec56906bed20457dba8",
+    "url": "/travis-react/index.html"
+  },
+  {
+    "revision": "6231769c824615639e3a",
+    "url": "/travis-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a7031bf3d4003d3d6993",
+    "url": "/travis-react/static/js/2.cb04cbdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/travis-react/static/js/2.cb04cbdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6231769c824615639e3a",
+    "url": "/travis-react/static/js/main.375a7c42.chunk.js"
+  },
+  {
+    "revision": "1bcad4185f82f9b73546",
+    "url": "/travis-react/static/js/runtime-main.93c427b7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/travis-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
