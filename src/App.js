07da59e761f424f4c1a360f2193c613433e0ffb3
akhan <akhan@renewfinancial.com>
2020-05-25 09:43:39 -0700

    Some changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          A demo react app for Travis configuration for git hub pages.
         </p>
         <a
           className="App-link"
--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/A demo react app for Travis configuration for git hub pages/i);
   expect(linkElement).toBeInTheDocument();
 });
